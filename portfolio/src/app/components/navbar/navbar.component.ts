c6a8ae205f08405ef88774889ee317c87e54f3b9
taianto <antont21c@gmail.com>
2024-05-17 00:33:12 +0100

    Improved navbar

--- portfolio/src/app/components/navbar/navbar.component.ts
+++ portfolio/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
