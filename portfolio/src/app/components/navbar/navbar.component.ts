410bc5c26c511986b6745a8e968cfb43641bf863
taianto <antont21c@gmail.com>
2024-05-16 17:30:40 +0100

    Organised Routing Structure

---
+++ portfolio/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterModule, CommonModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  navLinks = [
+    { label: 'Home', path: '' },
+    { label: 'About', path: '/about' },
+    { label: 'Portfolio', path: '/portfolio' },
+    { label: 'Contact', path: '/contact' }
+  ];
+}
