becee137096c75344ab7af571b6de77bf6b64c43
taianto <antont21c@gmail.com>
2024-06-02 13:39:52 +0100

    First draft of home page complete

--- portfolio/src/app/pages/home/home.component.ts
+++ portfolio/src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatTabsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
